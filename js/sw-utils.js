8d762ee35cddcbb8d447643577eb271add77852a
Juan Manuel Del Angel Santiago <jmdelan2012@gmail.com>
2025-07-09 15:51:22 -0600

    add manifest.json and update index.html for PWA support

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -1,13 +1,26 @@
 
 /* exported actualizarCacheDinamico */
-function actualizarCacheDinamico(dynamicCache,req,res){
-
-    if(res.ok){
-        return caches.open(dynamicCache). then(cache => {
+// function actualizarCacheDinamico(dynamicCache,req,res){
+//
+//     if(res.ok){
+//         return caches.open(dynamicCache). then(cache => {
+//             cache.put(req, res.clone());
+//             return res.clone();
+//         });
+//     }else{
+//         return res;
+//     }
+// }
+function actualizarCacheDinamico(dynamicCache, req, res) {
+    if (!req.url.startsWith('http')) {
+        return res; // Ignora peticiones que no sean http/https
+    }
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
             cache.put(req, res.clone());
             return res.clone();
         });
-    }else{
+    } else {
         return res;
     }
 }
